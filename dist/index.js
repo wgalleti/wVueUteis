b56da26e55bdc9103429f71337275f37c2927aa2
William Galleti <william.galleti@gmail.com>
2019-11-06 10:50:29 -0400

    fix version

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,8 @@ const axios = require('../src/axios.js')
 const helpers = require('../src/helpers.js')
 
 module.export = {
-  store: store,
-  devexpress: devexpress,
-  axios: axios,
-  helpers: helpers
+  store,
+  devexpress,
+  axios,
+  helpers
 }
